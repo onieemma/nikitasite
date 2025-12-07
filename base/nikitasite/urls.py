590a09c221711cccf627418e3bc997e418c7af1b
emmanuel ikwegbu <141882011+onieemma@users.noreply.github.com>
2025-12-07 17:49:37 +0100

    Add files via upload

---
+++ base/nikitasite/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+
+
+    path('accounts/', include('allauth.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'static')
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
