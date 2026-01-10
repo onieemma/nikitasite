39b82c7e28c0f1dbbe87b3cf38d70a95e96eceb4
emmanuel ikwegbu <141882011+onieemma@users.noreply.github.com>
2026-01-10 06:53:29 -0800

    Update settings.py

--- nikitasite/settings.py
+++ nikitasite/settings.py
@@ -107,23 +107,15 @@ WSGI_APPLICATION = 'nikitasite.wsgi.application'
 # DATABASE
 # ==============================================================================
 
-DATABASE_URL = config('DATABASE_URL', default=None)
 
-if DEBUG or not DATABASE_URL:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # This is the SQLite file
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
+}
+
 
 # ==============================================================================
 # PASSWORD VALIDATION
@@ -383,4 +375,5 @@ if not DEBUG:
             'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
         }
     }
+
 
