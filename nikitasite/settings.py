08e749c7baa066fd67e9d7f309b2161060efa9db
emmanuel ikwegbu <141882011+onieemma@users.noreply.github.com>
2026-01-10 06:47:13 -0800

    Update settings.py

--- nikitasite/settings.py
+++ nikitasite/settings.py
@@ -29,6 +29,7 @@ if not DEBUG:
     SECURE_HSTS_PRELOAD = True
     X_FRAME_OPTIONS = 'DENY'
 
+
 # ==============================================================================
 # SESSION SETTINGS
 # ==============================================================================
@@ -382,10 +383,4 @@ if not DEBUG:
             'LOCATION': config('REDIS_URL', default='redis://127.0.0.1:6379/1'),
         }
     }
-     SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    X_FRAME_OPTIONS = 'DENY'
+
